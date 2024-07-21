055abd37ac055fa6c0f41ddd3c8bed3c67c2a24b
Arun Kanaujia <arunkanaujia23@gmail.com>
2024-07-21 13:16:37 +0530

    responsive

--- src/Component/HeroSection/HeroSection.jsx
+++ src/Component/HeroSection/HeroSection.jsx
@@ -4,49 +4,63 @@ import styles from './heroSection.module.scss'
 const HeroSection = () => {
   const [isOpen, setIsOpen] = useState(false);
 
-    const toggleMenu = () => {
-        setIsOpen(!isOpen);
-    };
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <section className={`${styles?.heroSection}`}>
-         <div className={`container`}>
-            <header>
-            <div className="toggle-button" onClick={toggleMenu}>
-                ☰
+      <div className={`container`}>
+        <header>
+          <div className={styles?.toggleButtonsAndLoginContainer}>
+            <div className={styles?.toggleMenu} onClick={toggleMenu}>
+              ☰
             </div>
-                <nav >
-                    {
-                      isOpen ? (
-                       `ffff`
-                      ) : (
-                        <nav >
-                        <ul className={styles?.navList}>
-                        <li className={styles?.navListItem}><a href="#">Çözüm ve Hizmetler</a></li>
-                        <li className={styles?.navListItem}><a href="#">Ürünler</a></li>
-                        <li className={styles?.navListItem}><a href="#">Teknolojiler</a></li>
-                        <li className={styles?.navListItem}><a href="#"> İnsan Kaynakları</a></li>
-                        <li className={styles?.navListItem}><a href="#">Kurumsal</a></li>
-                        <li className={styles?.navListItemButton}><a href="#">İletişim</a></li>
-                    </ul>
-                    </nav>
-                      )
-                    }
-                    
-                </nav>
-            </header>
-            <div className={`${styles?.heroTextContainer}`}>
-                <h1 className={`${styles?.heroHeading}`}>Bilgi Teknolojilerinde
-                23 Yıllık Tecrübe</h1>
-                <p className={`${styles?.heroSubHeading}`}>
-                Müşterilerimizin yüksek kalite seviyelerini koruyabilmeleri için farklı sektörlerde tecrübe kazanmış uzman kadrolarımızla Proje Yönetimi, İş Analizi ve Test Yönetimi hizmetleri sunmaktayız.
-                </p>
+            <div className={styles?.navListItemButton}><a href="#">İletişim</a></div>
+          </div>
 
-                <div className={styles?.inputContainer}>
-                        <input className={styles?.input} type="text" placeholder='Mail bültenimize kayıt ol'/>
-                        <input className={styles?.inputButton} type="submit" value="Kayıt Ol"  />
-                </div>
-            </div>
-         </div>
+
+          {
+            isOpen ? (
+              <nav className={`${styles?.navBarMobile} ${toggleMenu ? styles?.active : ``}`} >
+                <ul className={styles?.navList}>
+                  <li onClick={toggleMenu} className={styles?.navListItem}><a href="#">Çözüm ve Hizmetler</a></li>
+                  <li onClick={toggleMenu} className={styles?.navListItem}><a href="#">Ürünler</a></li>
+                  <li onClick={toggleMenu} className={styles?.navListItem}><a href="#">Teknolojiler</a></li>
+                  <li onClick={toggleMenu} className={styles?.navListItem}><a href="#"> İnsan Kaynakları</a></li>
+                  <li onClick={toggleMenu} className={styles?.navListItem}><a href="#">Kurumsal</a></li>
+
+                </ul>
+              </nav>
+
+            ) : (
+              <nav className={styles?.navBarDesktop} >
+                <ul className={styles?.navList}>
+                  <li className={styles?.navListItem}><a href="#">Çözüm ve Hizmetler</a></li>
+                  <li className={styles?.navListItem}><a href="#">Ürünler</a></li>
+                  <li className={styles?.navListItem}><a href="#">Teknolojiler</a></li>
+                  <li className={styles?.navListItem}><a href="#"> İnsan Kaynakları</a></li>
+                  <li className={styles?.navListItem}><a href="#">Kurumsal</a></li>
+                  <li className={styles?.navListItemButton}><a href="#">İletişim</a></li>
+                </ul>
+              </nav>
+            )
+          }
+
+
+        </header>
+        <div className={`${styles?.heroTextContainer}`}>
+          <h1 className={`${styles?.heroHeading}`}>Bilgi Teknolojilerinde
+            23 Yıllık Tecrübe</h1>
+          <p className={`${styles?.heroSubHeading}`}>
+            Müşterilerimizin yüksek kalite seviyelerini koruyabilmeleri için farklı sektörlerde tecrübe kazanmış uzman kadrolarımızla Proje Yönetimi, İş Analizi ve Test Yönetimi hizmetleri sunmaktayız.
+          </p>
+
+          <div className={styles?.inputContainer}>
+            <input className={styles?.input} type="text" placeholder='Mail bültenimize kayıt ol' />
+            <input className={styles?.inputButton} type="submit" value="Kayıt Ol" />
+          </div>
+        </div>
+      </div>
     </section>
   )
 }
